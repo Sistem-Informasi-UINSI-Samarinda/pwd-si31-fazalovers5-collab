73bb8d1ccea3b6bf0394c18a567e9d7f41ec148e
fazalovers5-collab <fazalovers5@gmail.com>
2025-11-12 15:10:30 +0800

    praktikum 1

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
     
     <?php 
         $json = file_get_contents ('https://gist.githubusercontent.com/ihgoyarp/9327fcd326756747e042bdc499ceef01/raw/235202678a029d072369806a9781b89f36707ab9/SI32_PWD_2025');
-        $data = json_decode ($json, TRUE);
+        $data = json_decode($json, TRUE);
     ?>
     <section>
         <table>
             <tr>
-                <th>no</th>
-                <th>nim</th>
-                <th>nama</th>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
             </tr>
-            <?php foreach ($data as $key => $value); ?>
+            <?php foreach($data as $key => $value): ?>
             <tr>
-                <td><?php echo $value['no']?></td>
-                <td><?php echo $value['nim']?></td>
-                <td><?php echo $value['nama']?></td>
+                <td><?php echo $value['No']?></td>
+                <td><?php echo $value['NIM']?></td>
+                <td><?php echo $value['Nama']?></td>
             </tr>
             <?php endforeach; ?>
         </table>
